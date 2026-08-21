5ea62bf42b6cfd32203d6cf11caf8ad5083b7dba
a_budny <127764960+AdiKungen@users.noreply.github.com>
2026-08-21 19:41:11 +0200

    Poprawa zad 11 zestaw 2

--- zestaw 2 21.10/zad 11 21.10 F11/src/zad/pkg11/pkg21/pkg10/f11/Zad112110F11.java
+++ zestaw 2 21.10/zad 11 21.10 F11/src/zad/pkg11/pkg21/pkg10/f11/Zad112110F11.java
@@ -3,26 +3,20 @@ import java.util.Scanner;
 
 public class Zad112110F11 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         
         System.out.println("Podaj liczbę: ");
         int n = in.nextInt();
-        int ilosc = 0;
-        
-        ilosc = n - 1 + n;
         
         for (int i = 0; i < n; i++) {
-            int gwiazdki = 1;
-            for (i = 1; i < (ilosc-1)/2; i=i+2) {
-                System.out.println(" ");
+            for (int j = 0; j < n - 1 - i; j++) {
+                System.out.print(" ");
             }
-            for (i = 0; i < gwiazdki; i++) {
-                System.out.println("*");
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
             }
-            
-            
+            System.out.println();
         }
     }
-    
 }
